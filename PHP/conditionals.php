264dc947ca3733e351779cdde9eace8fe28729d2
cynthia.quach <ccquach93@gmail.com>
2018-07-24 09:30:56 -0700

    if-else statements

---
+++ PHP/conditionals.php
@@ -0,0 +1,21 @@
+<?php
+
+$user = 'andy';
+
+if ($user == 'rob') {
+  echo 'Hello Rob!';
+} else {
+  echo "I don't know you";
+}
+
+echo '<br><br>';
+
+$age = 19;
+
+if ($age >= 18 || $user == 'rob') {
+  echo 'You may proceed';
+} else {
+  echo 'You are too young!';
+}
+
+?>
